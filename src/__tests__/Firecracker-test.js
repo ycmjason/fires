b32b514507daaaa78c67c89770ba538341023e81
Jason Yu <me@ycmjason.com>
2018-12-24 17:10:38 +0000

    added encrypted firebase service account key

--- src/__tests__/Firecracker-test.js
+++ src/__tests__/Firecracker-test.js
@@ -1,4 +1,12 @@
 import Firecracker from '../Firecracker';
+import * as admin from 'firebase-admin';
+
+import serviceAccount from '../../firebaseServiceAccountKey.json';
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://firecracker-test-17604.firebaseio.com',
+});
 
 describe('Firecracker', () => {
   it('should be defined', () => {
