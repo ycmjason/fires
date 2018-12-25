1843ebb67dc44016a479acca1810302bb9ef34a1
Jason Yu <me@ycmjason.com>
2018-12-25 00:39:23 +0000

    move before hook

--- src/__tests__/FirecrackerCollection-test.js
+++ src/__tests__/FirecrackerCollection-test.js
@@ -14,7 +14,6 @@ describe('FirecrackerCollection', () => {
 
   describe('firecrackerCollection.findById(id)', () => {
     it('should call FirecrackerDocument.from($docRef)', async () => {
-      await $collection.doc('a').set({ value: 30 });
       const collection = new FirecrackerCollection($collection);
       FirecrackerDocument.from.mockResolvedValue('yoyo');
 
