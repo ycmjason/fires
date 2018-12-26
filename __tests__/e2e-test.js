6dfca71095d9106e8492a31b27c15c9cc1175b6e
Jason Yu <me@ycmjason.com>
2018-12-26 23:53:11 +0000

    test: added tests for reading from collection

--- __tests__/e2e-test.js
+++ __tests__/e2e-test.js
@@ -5,6 +5,8 @@ import firecracker, {
   FirecrackerDocument,
 } from '..';
 
+jest.setTimeout(10000);
+
 describe('e2e - CRUD', () => {
   let db;
   let $collection;
@@ -36,6 +38,36 @@ describe('e2e - CRUD', () => {
         expect($querySnapshot.docs.length).toBe(1);
         expect($querySnapshot.docs[0].id).toBe('wow');
         expect($querySnapshot.docs[0].data().x).toBe(5);
+      });
+    });
+  });
+
+  describe('Read', () => {
+    beforeEach(async () => {
+      await $collection.add({ count: 100 });
+      await $collection.add({ hello: 'world' });
+
+      await $collection.doc('someId').set({ yo: false });
+    });
+
+    describe('FirecrackerCollection.findById', () => {
+      it('should find the document with a specific ID', async () => {
+        const doc = await db.collection('e2e').findById('someId');
+        expect(doc).toBeInstanceOf(FirecrackerDocument);
+        expect(doc.yo).toBe(false);
+      });
+    });
+
+    describe('FirecrackerCollection.findAll', () => {
+      it('should retrieve all documents', async () => {
+        const docs = await db.collection('e2e').findAll('someId');
+        expect(docs).toBeInstanceOf(Array);
+        expect(docs.length).toBe(3);
+        expect(docs).toEqual(expect.arrayContaining([
+          expect.objectContaining({ count: 100 }),
+          expect.objectContaining({ hello: 'world' }),
+          expect.objectContaining({ yo: false }),
+        ]));
       });
     });
   });
