320df97b9b33cd811c787e68568ee9ced5a8b0cf
Jason Yu <me@ycmjason.com>
2018-12-27 12:44:11 +0000

    load .env in jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 const { resolve } = require('path');
 
+require('dotenv').config(); // load .env
 const dotEnvName = `.env.${process.env.NODE_ENV || 'test'}.local`;
-require('dotenv').config({ path:  resolve(process.cwd(), dotEnvName) });
+require('dotenv').config({ path: resolve(process.cwd(), dotEnvName) });
 
 module.exports = {
   testMatch: ['**/__tests__/**/*-test.js'],
