a2654eb2e2b101cdd99079a3e0e768e5dfc210d6
Jason Yu <me@ycmjason.com>
2019-01-19 22:43:52 +0000

    updated coverageIgnorePaths

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 module.exports = {
   testMatch: ['**/__tests__/**/*-test.js'],
-  coverageDirectory: './coverage/',
   collectCoverage: false,
+  coverageDirectory: './coverage/',
+  coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/'],
 };
